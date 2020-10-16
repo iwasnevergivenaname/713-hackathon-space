c28b60488d2ea6096e7f6e838482baa6ac45c833
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-10-16 16:06:58 -0700

    planet

--- src/components/Planet/Planet.js
+++ src/components/Planet/Planet.js
@@ -1,11 +1,11 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import './Planet.css'
 import logo from './logo.png'
 import play from './play.png'
 import rectangle from './Rectangle 12.png'
 import mercuryBig from './mercurybig.png'
 import mercurySmall from './mercurysmall.png'
-import mercuryTxt from './Marstxt.png'
+import mercuryTxt from './Mercurytxt.png'
 import venusSmall from './venusmall.png'
 import venusBig from './sqvenus.png'
 import venusTxt from './venustextokay.png'
@@ -62,28 +62,55 @@ export default function PlanetPage() {
   let [mainText, setMainText] = useState(Earth.txt)
   let [rightText, setRightText] = useState(Mars.txt)
 
-  let [facts, setFacts] =useState(Earth.facts)
+  let [facts, setFacts] = useState(Earth.facts)
+
+  let [leftAlt, setLeftAlt] = useState(Venus.name)
+  let [rightAlt, setRightAlt] = useState(Mars.name)
+
+  let [isOpened, setIsOpened] = useState(false)
 
 
   const planetSwitch = (e) => {
     if (e.target.alt === 'mars') {
       setLeftPhoto(Earth.small)
       setLeftText(Earth.txt)
+      setLeftAlt(Earth.name)
       setMainPhoto(Mars.big)
       setMainText(Mars.txt)
       setFacts(Mars.facts)
       setRightPhoto(Jupiter.small)
       setRightText(Jupiter.txt)
+      setRightAlt(Jupiter.alt)
+      setIsOpened({isOpened: !isOpened})
+      console.log('clicked mars')
     } else if (e.target.alt === 'venus') {
       setLeftPhoto(Mercury.small)
       setLeftText(Mercury.txt)
+      setLeftAlt(Mercury.name)
       setMainPhoto(Venus.big)
       setMainText(Venus.txt)
       setFacts(Venus.facts)
       setRightPhoto(Earth.small)
       setRightText(Earth.txt)
+      setRightAlt(Earth.name)
+      setIsOpened({isOpened: !isOpened})
+      console.log('clicked venus')
+    } else if (e.target.alt === 'earth') {
+      setLeftPhoto(Venus.small)
+      setLeftText(Venus.txt)
+      setLeftAlt(Venus.alt)
+      setMainPhoto(Earth.big)
+      setMainText(Earth.txt)
+      setFacts(Earth.facts)
+      setRightPhoto(Mars.small)
+      setRightText(Mars.txt)
+      setRightAlt(Mars.alt)
+      setIsOpened({isOpened: !isOpened})
+      console.log('clicked earth')
     }
   }
+
+
 
   return (
     <>
@@ -94,34 +121,34 @@ export default function PlanetPage() {
         <a href="../"><img src={logo} alt="logo"/></a>
       </div>
       <div className='left-planet' onClick={planetSwitch}>
-        <img src={leftPhoto} alt="venus"/>
+        <img src={leftPhoto} alt={leftAlt}/>
       </div>
       <div className='lefttext'>
-        <img src={leftText} alt="venus text"/>
+        <img src={leftText} alt={leftAlt}/>
       </div>
       <div className='main-planet'>
-        <img src={mainPhoto} alt="earth"/>
+        <img src={mainPhoto} alt="planet"/>
       </div>
       <div className='textshadow'>
         <img src={rectangle} alt="text shadow"/>
       </div>
       <div className='maintext'>
-        <img src={mainText} alt="earth text"/>
+        <img src={mainText} alt="main photo"/>
       </div>
       <div className='right-planet' onClick={planetSwitch}>
-        <img src={rightPhoto} alt="mars"/>
+        <img src={rightPhoto} alt={rightAlt}/>
       </div>
       <div className='facts'>
         <img src={facts} alt="facts"/>
       </div>
       <div className='righttext'>
-        <img src={rightText} alt="mars text"/>
-      </div>
-      <div className='guy'>
-        <img src={astronaut} alt="astronaut"/>
+        <img src={rightText} alt={rightAlt}/>
       </div>
       <div className='planet-page-moon'>
         <img src={moon} alt="moon"/>
+      </div>
+      <div className='guy'>
+        <img src={astronaut} alt="astronaut"/>
       </div>
     </>
   )
