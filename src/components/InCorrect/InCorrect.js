1b90452fc665de638fe0934890f4598736f55397
ekieff <kiefferelaine@gmail.com>
2020-10-16 16:03:41 -0500

    add timeout to status pages

--- src/components/InCorrect/InCorrect.js
+++ src/components/InCorrect/InCorrect.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useEffect}from 'react'
 
-function InCorrect(){
+function InCorrect(props){
+    useEffect(() =>{
+        setTimeout(() =>props.history.push('/play'), (5000))
+    })
     return(
         <div>
             <h1>That was wrong!</h1>
